f45b7b0cf9d8d26d8e4c738124c935c2de8474f1
davidbarbi3r <davidbarbi3r@gmail.com>
2024-05-31 16:27:00 +0200

    feat: Start Ohmyoga project

---
+++ Ohmyoga/Ohmyoga.Api/Program.cs
@@ -0,0 +1,19 @@
+using Ohmyoga.Application;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddApplication();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.Run();
